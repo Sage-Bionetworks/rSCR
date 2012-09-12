859dac9a4b117efaed81bf1a81913396726f987c
brigmecham <brig.mecham@sagebase.org>
2012-09-12 15:34:07 -0700

    Changed

--- R/handleContribution.R
+++ R/handleContribution.R
@@ -35,6 +35,34 @@ setMethod(
 
 setMethod(
 		f = "handleContribution",
+		signature = signature("Folder", "list","character"),
+		definition = function(project, contribution, logFile) {
+			# First remove any annotations set to NA
+			if(sum(is.na(contribution))){
+				contribution <- contribution[-which(is.na(contribution))]
+			}
+			# Next, make sure the user has provided the required information.
+			if(! all(c("data.url","study.name","data.name", "data.type","data.status") %in% names(contribution))){				
+				cat(setdiff(c("data.url","study.name","data.name", "data.type","data.status"), names(contribution)),"\n")
+				stop("Mising one of study.name, data.url, data.name, data.type, or data.status.  Please try again.\n")
+			}
+			# Set the data.lastUpdate to NA if it wasn't provided
+			if(! "data.lastUpdate" %in% names(contribution)){
+				# Last update not included.  Set to NA
+				contribution$data.lastUpdate = format(Sys.time(), "%d-%m-%Y")
+			}
+			contribution$parentId <- propertyValue(project,"id")
+			if(any(names(contribution) == "tissue")){
+				names(contribution)[which(names(contribution) == "tissue")] <- 'tissueType'
+			}
+			if(any(names(contribution) == "platformName")){
+				names(contribution)[which(names(contribution) == "platformName")] <- 'platform'
+			}
+			return(contribution)
+		}
+)
+setMethod(
+		f = "handleContribution",
 		signature = signature("NULL", "data.frame", "character"),
 		definition = function(project, contribution, logFile){
 			# here we assume user has supplied a dataframe, with
