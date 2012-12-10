85641f4964ad2a97f0288a1f1cfc97f2b024e4b0
brig.mecham <brig.mecham@sagebase.org>
2012-12-10 08:06:01 -0800

    updates

--- R/contributeMetadata.R
+++ R/contributeMetadata.R
@@ -17,3 +17,33 @@
 	}
 }
 
+syn341502
+
+parentId <- 'syn1533995'
+parent <- getEntity(parentId)
+
+ent <- Data(list(name="GSE14996_metadata.tsv",parentId=parentId))
+annotValue(ent, 'fileSize') <- .prettifyFileSize(file.info('GSE14996_metadata.tsv')$size)
+ent <- addFile(ent, 'GSE14996_metadata.tsv')
+annotValue(ent, 'study') <- propertyValue(parent,'name')
+annotValue(ent, 'repository') <- annotValue(parent,'repository')
+annotValue(ent, 'status') <- 'metadata'
+
+
+ent <- loadEntity('syn1125641')
+dat <- exprs(ent$objects$eset)
+rownames(dat) <- gsub("_mt", "_eg", rownames(dat))
+dat <- round(dat,3)
+write.table(dat,file=paste(propertyValue(ent, 'name'),".tsv",sep=""),sep="\t",quote=FALSE)
+
+newEnt <- Data(list(name=paste(propertyValue(ent, 'name'),'tsv',sep="."), parentId="syn1533995"))
+propertyValue(newEnt, 'disease') <- propertyValue(ent, 'disease')
+propertyValue(newEnt, 'tissueType') <- propertyValue(ent, 'tissueType')
+propertyValue(newEnt, 'platform') <- propertyValue(ent, 'platform')
+propertyValue(newEnt, 'species') <- propertyValue(ent, 'species')
+propertyValue(newEnt, 'numSamples') <- propertyValue(ent, 'numSamples')
+annotations(newEnt) <- annotations(ent)
+newEnt <- addFile(newEnt,'GSE14996_pd.genomewidesnp.6_gene.tsv')
+annotValue(newEnt, 'fileSize') <- .prettifyFileSize(file.info(paste(propertyValue(ent, 'name'),'tsv',sep="."))$size)
+annotValue(newEnt, 'status') <- 'processed'
+
